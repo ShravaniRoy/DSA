0a2bb01e176047a73bb131329b6163b06fd3cda6
ShravaniRoy <shravani.thirunagari@gmail.com>
2025-02-27 10:13:32 +0530

    currying evalutation functions

--- Javascript/currying/curryingKArgsFunc.js
+++ Javascript/currying/curryingKArgsFunc.js
@@ -4,14 +4,37 @@
 function add(a, b){
     return a+b;
 }
-function curriedAdd(a){
-    return function (b){
-        return add(a,b);
+function multiply(a, b){
+    return a*b;
+}
+function divide(a, b){
+    return a/b;
+}
+function subtract(a, b){
+    return a-b;
+}
+function curriedEval(operator){
+    return function (a){
+        return function (b){
+            if(operator === 'add') return add(a,b)
+                else if(operator === 'subtract') return subtract(a,b)
+                    else if(operator === 'multiply') return multiply(a,b)
+                        else if(operator === 'divide') return divide(a,b)
+                    else return "invalid operator"
+        }
     }
 }
 
 // console.log(curriedAdd(2)(4));
-const reusableAdd = curriedAdd(4);
-console.log(reusableAdd(4));
-console.log(reusableAdd(5));
-console.log(reusableAdd(6));
+// const reusableAdd = curriedAdd(4);
+// console.log(reusableAdd(4));
+// console.log(reusableAdd(5));
+// console.log(reusableAdd(6));
+const evaluateAdd = curriedEval('add');
+console.log(evaluateAdd(2)(3));
+console.log(evaluateAdd(5)(2));
+console.log(evaluateAdd(6)(9));
+const evaluateMul = curriedEval('multiply');
+console.log(evaluateMul(2)(3));
+console.log(evaluateMul(5)(2));
+console.log(evaluateMul(6)(9));
