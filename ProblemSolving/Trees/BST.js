ac16d30c73da9c30ee06b55ada38bc72e0f18775
ShravaniRoy <shravani.thirunagari@gmail.com>
2025-02-28 12:58:40 +0530

    implemented find function for Binary Search Tree

--- ProblemSolving/Trees/BST.js
+++ ProblemSolving/Trees/BST.js
@@ -1,52 +1,67 @@
 //A tree has one root node, has left and right nodes, leaves have no children
 //A Binary Tree is tree, where it has atmost 2 children
-//BST is a Binary Tree, where it has atmost 2 children but always left is smaller than 
+//BST is a Binary Tree, where it has atmost 2 children but always left is smaller than
 //parent and right is always greater than parent
 
 class BinarySearchTree {
-    constructor(){
-        this.root = null;
-    }
-    insert(val){
-        var newNode = new Node(val);
-        if(this.root === null){
-            this.root = newNode;
+  constructor() {
+    this.root = null;
+  }
+  insert(val) {
+    var newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      //we will traverse the tree to place the node in the correct position
+      var current = this.root;
+      while (true) {
+        if (current.value === val) return this;
+        if (current.value > val) {
+          if (current.left === null) {
+            current.left = newNode;
             return this;
+          }
+          //if there is a left node already, then we move to that position for checking further
+          current = current.left;
         } else {
-            //we will traverse the tree to place the node in the correct position
-            var current = this.root;
-            while(true){
-                if(current.value === val) return undefined;
-                if(current.value > val){
-                    if(current.left === null){
-                        current.left = newNode;
-                        return this;
-                    }  
-                //if there is a left node already, then we move to that position for checking further
-                current = current.left;
-                    
-                } 
-                else {
-                    if(current.right === null){
-                        current.right = newNode;
-                        return this;
-                    }
-                    current = current.right;
-                    
-                }
-            }
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          }
+          current = current.right;
+        }
+      }
+    }
+  }
+  find(val){
+    if(this.root === null) return -1;
+    if(this.root.value === val) return 1;
+    var currentNode = this.root;
+    var found = -1;
+    while(currentNode && found === -1){
+        if(currentNode.value > val){
+            currentNode = currentNode.left;
+        }
+        else if(currentNode.value < val){
+            currentNode = currentNode.right;
+        }
+        else {
+            found = 1;
         }
     }
+    return found;
+  }
 }
 class Node {
-    constructor(){
-        this.left = null;
-        this.right = null;
-        this.value = null;
-    }
+  constructor(val) {
+    this.left = null;
+    this.right = null;
+    this.value = val;
+  }
 }
 
-var tree = BinarySearchTree();
+var tree = new BinarySearchTree();
 tree.insert(10);
 tree.insert(5);
 tree.insert(13);
@@ -55,3 +70,5 @@ tree.insert(2);
 tree.insert(16);
 tree.insert(7);
 tree.insert(1);
+tree.find(7);
+tree.find(70);
