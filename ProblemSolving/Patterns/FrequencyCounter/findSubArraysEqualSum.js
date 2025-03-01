79c622449c48d8ad567e84922ac4efdf3ecd811c
ShravaniRoy <shravani.thirunagari@gmail.com>
2025-03-01 12:27:13 +0530

    printing sub array thats have equal sum

--- ProblemSolving/Patterns/FrequencyCounter/findSubArraysEqualSum.js
+++ ProblemSolving/Patterns/FrequencyCounter/findSubArraysEqualSum.js
@@ -1,21 +1,27 @@
-// find sub arrays of size k that have equal sum 
+// find sub arrays of size k that have equal sum
 
-function subArraySumFreq(arr, k){
-    let subArrSumFreq = {};
-    let tempsum =  0;
-    //compute the first k elements sum
-    for(let i=0; i<k; i++){
-        tempsum += arr[i];
+function subArraySumFreq(arr, k) {
+  let subArrSumFreq = {};
+  let tempsum = 0;
+  //compute the first k elements sum
+  for (let i = 0; i < k; i++) {
+    tempsum += arr[i];
+  }
+  subArrSumFreq[tempsum] = [[...arr.slice(0, k)]];
+  //sliding window for next sum frequencies
+  for (let i = k; i < arr.length; i++) {
+    tempsum = tempsum - arr[i - k] + arr[i];
+    let currentSubArray = [...arr.slice(i - k + 1, i + 1)]; //extract current sub array
+    if (subArrSumFreq.hasOwnProperty(tempsum)) {
+      subArrSumFreq[tempsum].push(currentSubArray);
+    } else {
+      subArrSumFreq[tempsum] = [currentSubArray];
     }
-    subArrSumFreq[tempsum] = 1;
-    //sliding window for next sum frequencies
-    for(let i=k; i<arr.length; i++){
-        tempsum = tempsum - arr[i - k] + arr[i];
+  }
 
-        subArrSumFreq[tempsum] = (subArrSumFreq[tempsum] || 0) + 1;
-    }
-    
-    return subArrSumFreq;
+  for (let each in subArrSumFreq) {
+    console.log(`Sum ` + each, subArrSumFreq[each]);
+  }
 }
 
-console.log(subArraySumFreq([1, 2, 3, 4, 5, 6], 3));
+subArraySumFreq([1, 2, 3, 4, 5, 6, 2, 3, 4], 3);
